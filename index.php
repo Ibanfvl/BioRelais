ecba121d88c21c78a9a6f344f1619adbbfb2d6f0
Fraval Iban <102430546+Ibanfvl@users.noreply.github.com>
2026-05-04 10:49:32 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+require 'lib/loader.php';
+session_start()?>
+
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8" />
+		<title>bioRelai</title>
+		<style type="text/css">
+			@import url(styles/bioRelai.css);
+		</style>
+	</head>
+	<body>
+		<?php
+			include_once 'controleurs/controleurPrincipal.php';
+		?>
+		<script src = 'scripts/scripts.js'></script>
+	</body>
+</html>
